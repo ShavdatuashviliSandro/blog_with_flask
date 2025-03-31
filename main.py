3e468e5b8eb55230f735869d0a60d7f72154d725
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2025-03-31 14:45:34 +0400

    added jinja

--- main.py
+++ main.py
@@ -10,5 +10,11 @@ def index():
 def about_us():
     return render_template('about_us.html')
 
+@app.route('/contact')
+def contact():
+    item = [{'id': 1, 'name': 'givi', 'email': 'sandro@gmail.com'},
+            {'id': 2, 'name': 'luka', 'email': 'sandro@gmail.com'}]
+
+    return render_template('contact.html', items = item)
 if __name__ == '__main__':
     app.run(debug=True)
