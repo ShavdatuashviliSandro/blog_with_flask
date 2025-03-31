39447d8f9f4dfa076577ad5f3a216cf9ac5d593c
SandroSH <sandro.shav@gmail.com>
2025-03-31 19:41:17 +0400

    added jinja test

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+cards = [
+             {'title': 'ქართველი ჩემპიონები',
+             'description': 'ამ სტატიაში თქვენ წაიკითხავთ მოზარდების ძიუდოთი დაინტერესების დადებით თვისებებს.'},
+             {'title': 'მსოფლიო რეკორდები',
+              'description': 'ამ სტატიაში თქვენ წაიკითხავთ მსოფლიო რეკორდების შესახებ.'},
+             {'title': 'ქართველი ჩემპიონები',
+              'description': 'ამ სტატიაში თქვენ წაიკითხავთ მოზარდების ძიუდოთი დაინტერესების დადებით თვისებებს.'},
+             {'title': 'მსოფლიო რეკორდები',
+              'description': 'ამ სტატიაში თქვენ წაიკითხავთ მსოფლიო რეკორდების შესახებ.'},
+             {'title': 'მსოფლიო რეკორდები',
+              'description': 'ამ სტატიაში თქვენ წაიკითხავთ მსოფლიო რეკორდების შესახებ.'}
+             ]
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', cards=cards)
+
+@app.route('/add_item/<title>/<description>')
+def add_item(title,description):
+    cards.append({'title': title, 'description': description})
+    return render_template('index.html', cards=cards)
 
 @app.route('/about_us')
 def about_us():
@@ -12,8 +31,8 @@ def about_us():
 
 @app.route('/contact')
 def contact():
-    items = 1
-    return render_template('contact.html', items = items)
+    names = ['გიორგი', 'სანდრო', 'რეზი', 'ერეკლე', 'ლიზი']
+    return render_template('contact.html', names = names)
 
 if __name__ == '__main__':
     app.run(debug=True)
